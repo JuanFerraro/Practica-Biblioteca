829ce950f8eeabbb96460764176888b289fe1e57
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-23 18:44:18 -0500

    Cambios activados del gitignore

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://User:user@mongodb1.dz8ee6z.mongodb.net/biblioteca_municipal', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(db => console.log('DB esta conectada'))
-    .catch(err => console.error(err));
-
-
--- src/helpers/bcryptUtil.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-/* Encriptar contraseña */
-const encryptPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  const hash = bcrypt.hash(password, salt);
-  return hash;
-};
-
-module.exports = { encryptPassword };
